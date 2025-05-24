5658277c28f51e6df37f26bba0e10e6ba67545d4
MrSushi74 <jameskoh313@gmail.com>
2025-05-24 22:06:10 +0700

    added rightbar.css for the rightbar of home.php, added a href to the icon, added temp reccomended div inside right bar as well as  a searchbar

--- layout/home.php
+++ layout/home.php
@@ -6,12 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>SeaGram</title>
     <link rel="stylesheet" href="../CSS/sidebar.css" />
+    <link rel="stylesheet" href="../CSS/rightbar.css" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
   </head>
   <body>
     <div class="sidebar">
-       <svg class="icon" fill="#50b7f5" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 264.564 264.564" xml:space="preserve" stroke="#50b7f5"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M132.281,264.564c51.24,0,92.931-41.681,92.931-92.918c0-50.18-87.094-164.069-90.803-168.891L132.281,0l-2.128,2.773 c-3.704,4.813-90.802,118.71-90.802,168.882C39.352,222.883,81.042,264.564,132.281,264.564z"></path> </g> </g> </g></svg>
-      <div class="sidebarOption active">
+      <a href="../layout/home.php" class="svghover">
+       <svg class="icon" fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 264.564 264.564" xml:space="preserve" stroke="#50b7f5"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M132.281,264.564c51.24,0,92.931-41.681,92.931-92.918c0-50.18-87.094-164.069-90.803-168.891L132.281,0l-2.128,2.773 c-3.704,4.813-90.802,118.71-90.802,168.882C39.352,222.883,81.042,264.564,132.281,264.564z"></path> </g> </g> </g></svg>
+      </a>
+       <div class="sidebarOption active">
         <a href="#" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
         <span class="material-icons"> home </span>
         <h2>Home</h2>
@@ -52,8 +55,22 @@
 
     </div>
 
+    <div class="rightbar">
+    <!--search&follow-->
+      <form class= "searchcontainer" action="#">
+            <div class="search">
+                <span class="material-icons"> search </span>
+                <input class="search-input" type="search" placeholder="search">
+            </div>
+      </form>
 
-    <!--widget-->
-
+      <div class="reccomended">
+        <span class="text">this is for reccomended</span>
+      </div>
+        <div class="footer">
+          <hr>
+            <span>seagram 2025</span>
+        </div>
+    </div>
   </body>
 </html>
