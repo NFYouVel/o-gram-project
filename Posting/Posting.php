7cf21142d3cb7fcc113b1ec6530d009fb12954fd
ZeroGyoza <152766914+ZeroGyoza@users.noreply.github.com>
2025-05-24 19:20:37 +0700

    ubah css sama php

--- Posting/Posting.php
+++ Posting/Posting.php
@@ -13,19 +13,44 @@
 <body>
     <div class="table_posting">
         <div class="posting_card">
+
+
             <div class="user-header">
-                <p class="display-name">David</p>
-                <p class="username">@DavidGanteng</p>
-                <button class="follow-btn">Follow</button>
-                <img src="../../../../Pictures/Screenshots/Screenshot (14).png" alt="Foto Profil">
-            </div>
-                <img src="../../../../Pictures/Screenshots/Screenshot (1).png">
-                <div class="button_action">
-                    <button class="fa-regular fa-heart"></button>
-                    <button class="fa-regular fa-comment"></button>
-                    <button class="fa-regular fa-bookmark"></button>
-                    <button class="fa-solid fa-retweet"></button>
+                <div class="user-left">
+                    <img src="../Posting/pict/Screenshot (14).png" alt="Foto Profil">
+                    <div class="user-info">
+                        <p class="display-name">David</p>
+                        <p class="username">@DavidGanteng</p>
+                    </div>
                 </div>
+
+                <label class="follow-toggle">
+                    <input type="checkbox" hidden />
+                    <span class="follow-btn">Follow</span>
+                </label>
+            </div>
+
+            <img src="../Posting/pict/Screenshot (39).png" class="post-image">
+
+            <div class="button_action">
+                <label class="icon-toggle">
+                    <input type="checkbox" hidden>
+                    <span class="fa-regular fa-heart"></span>
+                </label>
+                <label class="icon-toggle">
+                    <input type="checkbox" hidden>
+                    <span class="fa-regular fa-comment"></span>
+                </label>
+                <label class="icon-toggle">
+                    <input type="checkbox" hidden>
+                    <span class="fa-regular fa-bookmark"></span>
+                </label>
+                <label class="icon-toggle">
+                    <input type="checkbox" hidden>
+                    <span class="fa-solid fa-retweet"></span>
+                </label>
+            </div>
+
         </div>
     </div>
 </body>
