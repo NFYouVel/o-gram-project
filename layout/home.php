eede5847676851a8b1c77e234304c8be8a3fab13
MrSushi74 <jameskoh313@gmail.com>
2025-05-24 16:05:12 +0700

    Finished Home Layout
    beda file jadi gw add baru

---
+++ layout/home.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>SeaGram</title>
+    <link rel="stylesheet" href="sidebar.css" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+  </head>
+  <body>
+    <div class="sidebar">
+       <svg class="icon" fill="#50b7f5" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 264.564 264.564" xml:space="preserve" stroke="#50b7f5"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M132.281,264.564c51.24,0,92.931-41.681,92.931-92.918c0-50.18-87.094-164.069-90.803-168.891L132.281,0l-2.128,2.773 c-3.704,4.813-90.802,118.71-90.802,168.882C39.352,222.883,81.042,264.564,132.281,264.564z"></path> </g> </g> </g></svg>
+      <div class="sidebarOption active">
+        <a href="#" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
+        <span class="material-icons"> home </span>
+        <h2>Home</h2>
+        </a>
+      </div>
+
+      <div class="sidebarOption">
+        <a href="#" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
+        <span class="material-icons"> search </span>
+        <h2>Explore</h2>
+        </a>
+      </div>
+
+      <div class="sidebarOption">
+        <a href="#" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
+        <span class="material-icons"> bookmark </span>
+        <h2>Bookmarks</h2>
+        </a>
+      </div>
+
+      <div class="sidebarOption">
+        <a href="#" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
+        <span class="material-icons"> perm_identity </span>
+        <h2>Profile</h2>
+        </a>
+      </div>
+
+      <div class="sidebarOption">
+        <a href="#" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
+        <span class="material-icons"> settings </span>
+        <h2>Settings</h2>
+        </a>
+      </div>
+    </div>
+
+    <!-- posting -->
+    <div class="posts">
+
+    </div>
+
+
+    <!--widget-->
+
+  </body>
+</html>
