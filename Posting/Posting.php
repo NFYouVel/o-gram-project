91b1a2a58b0b4d65681e70f130e2fe87673255b4
NFYouVel <marvelnathanael072006@gmail.com>
2025-05-27 09:06:25 +0700

    27 05

--- Posting/Posting.php
+++ Posting/Posting.php
@@ -11,43 +11,40 @@
 </head>
 
 <body>
-
-    <div class="table_posting">
-        <div class='posting_card'>
-            <div class="user-header">
-                <div class="user-left">
-                    <img src="../Posting/pict/Screenshot (14).png" alt="Foto Profil">
-                    <div class="user-info">
-                        <p class="display-name">David</p>
-                        <p class="username">@DavidGanteng</p>
-                    </div>
+    <div class='posting_card'>
+        <div class="user-header">
+            <div class="user-left">
+                <img src="../Posting/pict/Screenshot (14).png" alt="Foto Profil">
+                <div class="user-info">
+                    <p class="display-name">David</p>
+                    <p class="username">@DavidGanteng</p>
                 </div>
-
-                <label class="follow-toggle">
-                    <input type="checkbox" hidden />
-                    <span class="follow-btn">Follow</span>
-                </label>
             </div>
-            <img src="../Posting/pict/Screenshot (39).png" class="post-image">
 
-            <div class="button_action">
-                <label class="icon-toggle">
-                    <input type="checkbox" hidden>
-                    <span class="fa-regular fa-heart"></span>
-                </label>
-                <label class="icon-toggle">
-                    <input type="checkbox" hidden>
-                    <span class="fa-regular fa-comment"></span>
-                </label>
-                <label class="icon-toggle">
-                    <input type="checkbox" hidden>
-                    <span class="fa-regular fa-bookmark"></span>
-                </label>
-                <label class="icon-toggle">
-                    <input type="checkbox" hidden>
-                    <span class="fa-solid fa-retweet"></span>
-                </label>
-            </div>
+            <label class="follow-toggle">
+                <input type="checkbox" hidden />
+                <span class="follow-btn">Follow</span>
+            </label>
+        </div>
+        <img src="../Posting/pict/Screenshot (39).png" class="post-image">
+
+        <div class="button_action">
+            <label class="icon-toggle">
+                <input type="checkbox" hidden>
+                <span class="fa-regular fa-heart"></span>
+            </label>
+            <label class="icon-toggle">
+                <input type="checkbox" hidden>
+                <span class="fa-regular fa-comment"></span>
+            </label>
+            <label class="icon-toggle">
+                <input type="checkbox" hidden>
+                <span class="fa-regular fa-bookmark"></span>
+            </label>
+            <label class="icon-toggle">
+                <input type="checkbox" hidden>
+                <span class="fa-solid fa-retweet"></span>
+            </label>
         </div>
 </body>
 
--- layout/settings.php
+++ layout/settings.php
@@ -1,3 +1,8 @@
+<?php
+  $id = $_GET['id'];
+  $temp = "?id=".$id;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -23,25 +28,25 @@
       }
     </script>
     <div class="sidebar">
-      <a href="../layout/home.php" class="svghover">
+      <a href="../layout/home.php<?php echo $temp ?>" class="svghover">
        <svg class="icon" fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 264.564 264.564" xml:space="preserve" stroke="#50b7f5"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M132.281,264.564c51.24,0,92.931-41.681,92.931-92.918c0-50.18-87.094-164.069-90.803-168.891L132.281,0l-2.128,2.773 c-3.704,4.813-90.802,118.71-90.802,168.882C39.352,222.883,81.042,264.564,132.281,264.564z"></path> </g> </g> </g></svg>
       </a>
        <div class="sidebarOption">
-        <a href="../layout/home.php" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
+        <a href="../layout/home.php<?php echo $temp ?>" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
         <span class="material-icons"> home </span>
         <h2>Home</h2>
         </a>
       </div>
 
       <div class="sidebarOption">
-        <a href="../layout/search.php" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
+        <a href="../layout/search.php<?php echo $temp ?>" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
         <span class="material-icons"> search </span>
         <h2>Explore</h2>
         </a>
       </div>
 
       <div class="sidebarOption">
-        <a href="../layout/bookmark.php" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
+        <a href="../layout/bookmark.php<?php echo $temp ?>" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
         <span class="material-icons"> bookmark </span>
         <h2>Bookmarks</h2>
         </a>
