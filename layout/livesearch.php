fdb2c56b6d00ba590a3f754fdff3c09463d217eb
ZeroGyoza <152766914+ZeroGyoza@users.noreply.github.com>
2025-05-27 23:50:39 +0700

    livesearch update

--- layout/livesearch.php
+++ layout/livesearch.php
@@ -1,24 +1,35 @@
 <?php
-    include "../Connection/Connection.php";
-    if (isset($_POST['input'])) {
-        $input = $_POST['input'];
-        $query = "SELECT * FROM user WHERE username LIKE '%$input%'";
-        $sql = mysqli_query($connection,$query);
-        while ($row = mysqli_fetch_array($sql)) {
-        $name = $row["username"];
-        $nickname = $row["nickname"];
-        $pp = $row["profilepic"];
+include "../Connection/Connection.php";
+if (isset($_POST['input'])) {
+  $input = $_POST['input'];
+  $query = "SELECT * FROM user WHERE username LIKE '%$input%'";
+  $sql = mysqli_query($connection, $query);
 
-
-            echo "<div class='user-suggestion'>
-        <img src='pict/".htmlspecialchars($pp)."' alt='Profile 1' class='profile-img'>
+  while ($row = mysqli_fetch_array($sql)) {
+    $name = $row["username"];
+    $nickname = $row["nickname"];
+    if (isset($row['profilepic'])) {
+      echo "<div class='user-suggestion'>
+        <img src='pfp/" . $row['profilepic'] . "' alt='Profile 1' class='profile-img'>
         <div class='user-info'>
-          <p class='display-name'>".htmlspecialchars($name)."</p>
-          <p class='username'>@".htmlspecialchars($nickname)."</p>
+          <p class='display-name'>" . htmlspecialchars($name) . "</p>
+          <p class='username'>@" . htmlspecialchars($nickname) . "</p>
         </div>
         <input type='checkbox' id='follow1' class='follow-toggle hidden'>
         <label for='follow1' class='follow-btn' data-text='Follow' data-text-checked='Unfollow'></label>
       </div>";
-        }
     }
+    else{
+      echo "<div class='user-suggestion'>
+        <img src='pfp/avatar def.jpg' alt='Profile 1' class='profile-img'>
+        <div class='user-info'>
+          <p class='display-name'>" . htmlspecialchars($name) . "</p>
+          <p class='username'>@" . htmlspecialchars($nickname) . "</p>
+        </div>
+        <input type='checkbox' id='follow1' class='follow-toggle hidden'>
+        <label for='follow1' class='follow-btn' data-text='Follow' data-text-checked='Unfollow'></label>
+      </div>";
+    }
+  }
+}
 ?>
