08dacc2e5000a8b22f82ff372f504109476ea803
NFYouVel <marvelnathanael072006@gmail.com>
2025-05-27 09:22:43 +0700

    deleted html

--- layout/home.php
+++ layout/home.php
@@ -52,32 +52,44 @@ if (isset($_GET['input'])) {
   if ($row = mysqli_fetch_array($result)) {
     $id = $row['id'];
   }
-
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>SeaGram</title>
-    <link rel="stylesheet" href="../CSS/sidebar.css" />
-    <link rel="stylesheet" href="../CSS/rightbar.css" />
-    <link rel="stylesheet" href="../CSS/reccomended.css" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-    <!--googlejquery-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="../JS/ajaxlivesearch.js"></script>
-  </head>
-  <body>
-    <div class="sidebar">
-      <a href="../layout/home.php<?php echo $temp ?>" class="svghover">
-       <svg class="icon" fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 264.564 264.564" xml:space="preserve" stroke="#50b7f5"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M132.281,264.564c51.24,0,92.931-41.681,92.931-92.918c0-50.18-87.094-164.069-90.803-168.891L132.281,0l-2.128,2.773 c-3.704,4.813-90.802,118.71-90.802,168.882C39.352,222.883,81.042,264.564,132.281,264.564z"></path> </g> </g> </g></svg>
-      </a>
-       <div class="sidebarOption active">
-        <a style="display: flex; align-items: center; text-decoration: none; color: inherit;">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>SeaGram</title>
+  <link rel="stylesheet" href="../CSS/sidebar.css" />
+  <link rel="stylesheet" href="../CSS/rightbar.css" />
+  <link rel="stylesheet" href="../CSS/reccomended.css" />
+  <link rel="stylesheet" href="../CSS/midPost.css" />
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+  <!--googlejquery-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="../JS/ajaxlivesearch.js"></script>
+</head>
+
+<body>
+  <div class="sidebar">
+    <a href="../layout/home.php<?php echo $temp ?>" class="svghover">
+      <svg class="icon" fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 264.564 264.564" xml:space="preserve" stroke="#50b7f5">
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+        <g id="SVGRepo_iconCarrier">
+          <g>
+            <g>
+              <path d="M132.281,264.564c51.24,0,92.931-41.681,92.931-92.918c0-50.18-87.094-164.069-90.803-168.891L132.281,0l-2.128,2.773 c-3.704,4.813-90.802,118.71-90.802,168.882C39.352,222.883,81.042,264.564,132.281,264.564z"></path>
+            </g>
+          </g>
+        </g>
+      </svg>
+    </a>
+    <div class="sidebarOption active">
+      <a style="display: flex; align-items: center; text-decoration: none; color: inherit;">
         <span class="material-icons"> home </span>
         <h2>Home</h2>
       </a>
@@ -85,9 +97,9 @@ if (isset($_GET['input'])) {
 
     <div class="sidebarOption">
       <?php
-        $temp = "?id=".$id;
+      $temp = "?id=" . $id;
       ?>
-      <a href="../layout/search.php<?php echo $temp?>"
+      <a href="../layout/search.php<?php echo $temp ?>"
         style="display: flex; align-items: center; text-decoration: none; color: inherit;">
         <span class="material-icons"> search </span>
         <h2>Explore</h2>
@@ -95,7 +107,7 @@ if (isset($_GET['input'])) {
     </div>
 
     <div class="sidebarOption">
-      <a href="../layout/bookmark.php<?php echo $temp?>"
+      <a href="../layout/bookmark.php<?php echo $temp ?>"
         style="display: flex; align-items: center; text-decoration: none; color: inherit;">
         <span class="material-icons"> bookmark </span>
         <h2>Bookmarks</h2>
@@ -110,7 +122,7 @@ if (isset($_GET['input'])) {
     </div>
 
     <div class="sidebarOption">
-      <a href="../layout/settings.php<?php echo $temp?>"
+      <a href="../layout/settings.php<?php echo $temp ?>"
         style="display: flex; align-items: center; text-decoration: none; color: inherit;">
         <span class="material-icons"> settings </span>
         <h2>Settings</h2>
@@ -121,9 +133,11 @@ if (isset($_GET['input'])) {
   <!-- posting -->
   <div class="posts">
     <?php
-      include('../Posting/Posting.php');
+    
+    $query = "SELECT * FROM post"
+
+    
     ?>
-  </div>
   </div>
 
   <div class="rightbar">
