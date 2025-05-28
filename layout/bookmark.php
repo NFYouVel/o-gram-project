e95c46c845cf22435a4b1880dd4b2c9a9723daef
ZeroGyoza <152766914+ZeroGyoza@users.noreply.github.com>
2025-05-28 13:11:11 +0700

    update profile UX

--- layout/bookmark.php
+++ layout/bookmark.php
@@ -1,6 +1,7 @@
 <?php
   $id = $_GET['id'];
   $temp = "?id=".$id;
+  include("../Connection/Connection.php");
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +16,6 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
   </head>
   <body>
-    
     <div class="sidebar">
       <a href="../layout/home.php<?php echo $temp?>" class="svghover">
        <svg class="icon" fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 264.564 264.564" xml:space="preserve" stroke="#50b7f5"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M132.281,264.564c51.24,0,92.931-41.681,92.931-92.918c0-50.18-87.094-164.069-90.803-168.891L132.281,0l-2.128,2.773 c-3.704,4.813-90.802,118.71-90.802,168.882C39.352,222.883,81.042,264.564,132.281,264.564z"></path> </g> </g> </g></svg>
@@ -57,9 +57,15 @@
     </div>
 
     <!-- posting -->
-    <div class="posts">
+      <?php
+        $post_id = $_GET['post_id'];
+        $query = "SELECT * FROM post WHERE id = '$post_id'";
+        $result = mysqli_query($connection, $query);
 
-    </div>
+        while ($row = mysqli_fetch_array($result)) {
+          
+        }
+      ?>
 
     <div class="rightbar">
     <!--search&follow-->
--- layout/profile.php
+++ layout/profile.php
@@ -1,88 +1,156 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-    $temp = $_GET['id'];
+$temp = $_GET['id'];
 ?>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile</title>
-    <link rel="stylesheet" href="../CSS/profile.css" />
-     <link rel="stylesheet" href="../CSS/sidebar.css" />
-        <link rel="stylesheet" href="../CSS/rightbar.css" />
-        <link rel="stylesheet" href="../CSS/search.css" />
-          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Profile</title>
+  <link rel="stylesheet" href="../CSS/profile.css" />
+  <link rel="stylesheet" href="../CSS/sidebar.css" />
+  <link rel="stylesheet" href="../CSS/rightbar.css" />
+  <link rel="stylesheet" href="../CSS/search.css" />
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
 </head>
+
 <body>
-    <div class="sidebar">
-      <a href="../layout/home.php<?php echo $temp?>" class="svghover">
-       <svg class="icon" fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 264.564 264.564" xml:space="preserve" stroke="#50b7f5"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M132.281,264.564c51.24,0,92.931-41.681,92.931-92.918c0-50.18-87.094-164.069-90.803-168.891L132.281,0l-2.128,2.773 c-3.704,4.813-90.802,118.71-90.802,168.882C39.352,222.883,81.042,264.564,132.281,264.564z"></path> </g> </g> </g></svg>
-      </a>
-       <div class="sidebarOption">
-        <a href="../layout/home.php?id=<?php echo $temp?>" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
+  <div class="sidebar">
+    <a href="../layout/home.php<?php echo $temp ?>" class="svghover">
+      <svg class="icon" fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 264.564 264.564" xml:space="preserve" stroke="#50b7f5">
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+        <g id="SVGRepo_iconCarrier">
+          <g>
+            <g>
+              <path
+                d="M132.281,264.564c51.24,0,92.931-41.681,92.931-92.918c0-50.18-87.094-164.069-90.803-168.891L132.281,0l-2.128,2.773 c-3.704,4.813-90.802,118.71-90.802,168.882C39.352,222.883,81.042,264.564,132.281,264.564z">
+              </path>
+            </g>
+          </g>
+        </g>
+      </svg>
+    </a>
+    <div class="sidebarOption">
+      <a href="../layout/home.php?id=<?php echo $temp ?>"
+        style="display: flex; align-items: center; text-decoration: none; color: inherit;">
         <span class="material-icons"> home </span>
         <h2>Home</h2>
-        </a>
-      </div>
-
-      <div class="sidebarOption">
-        <a href="../layout/search.php?id=<?php echo $temp?>" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
-        <span class="material-icons"> search </span>
-        <h2>Explore</h2>
-        </a>
-      </div>
-
-      <div class="sidebarOption ">
-        <a href="../layout/bookmark.php?id=<?php echo $temp?>" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
-        <span class="material-icons"> bookmark </span>
-        <h2>Bookmarks</h2>
-        </a>
-      </div>
-
-      <div class="sidebarOption active">
-        <a style="display: flex; align-items: center; text-decoration: none; color: inherit;">
-        <span class="material-icons"> perm_identity </span>
-        <h2>Profile</h2>
-        </a>
-      </div>
-
-      <div class="sidebarOption">
-        <a href="../layout/settings.php?id=<?php echo $temp?>" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
-        <span class="material-icons"> settings </span>
-        <h2>Settings</h2>
-        </a>
-      </div>
+      </a>
     </div>
 
-    <?php
-    include("../Connection/Connection.php");
+    <div class="sidebarOption">
+      <a href="../layout/search.php?id=<?php echo $temp ?>"
+        style="display: flex; align-items: center; text-decoration: none; color: inherit;">
+        <span class="material-icons"> search </span>
+        <h2>Explore</h2>
+      </a>
+    </div>
+
+    <div class="sidebarOption ">
+      <a href="../layout/bookmark.php?id=<?php echo $temp ?>"
+        style="display: flex; align-items: center; text-decoration: none; color: inherit;">
+        <span class="material-icons"> bookmark </span>
+        <h2>Bookmarks</h2>
+      </a>
+    </div>
+
+    <div class="sidebarOption active">
+      <a style="display: flex; align-items: center; text-decoration: none; color: inherit;">
+        <span class="material-icons"> perm_identity </span>
+        <h2>Profile</h2>
+      </a>
+    </div>
+
+    <div class="sidebarOption">
+      <a href="../layout/settings.php?id=<?php echo $temp ?>"
+        style="display: flex; align-items: center; text-decoration: none; color: inherit;">
+        <span class="material-icons"> settings </span>
+        <h2>Settings</h2>
+      </a>
+    </div>
+  </div>
+
+  <?php
     if (isset($_GET['id'])) {
     $id = $_GET['id'];
+    ?>
+    <?php
+    include("../Connection/Connection.php");
     $query = "SELECT * FROM user WHERE id = '$id'";
     $result = mysqli_query($connection, $query);
 
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<div class='profile-bg'>";
-            echo "<div class='user-header'>";
-                echo "<div class='user-left'>";
-                    echo "<img src= 'pfp/" . $row['profilepic'] ."' alt='Foto Profile'>";
-                echo "</div>";
-                    echo "<div class='user-info'>";
-                        echo "<p class = 'display-name'>" . $row['nickname'] . "</p>";
-                        echo "<p class = 'username'>@" . $row['username'] . "</p>";
-                        echo "<br>" . "<p class = 'joined-date'>" . $row['created_at'] . "</p>";
-                        echo "</div>";
-            echo "</div>";
-    
-                echo "<label class = 'edit-toggle'>";
-                    echo "<a href = '#'>Edit Profile</a>";
-                echo"</label>";
-            echo "</div>";
-        echo "</div>";
+    while ($row = mysqli_fetch_array($result)) {
+      echo "<div class='profile-bg'>";
+      echo "<div class='user-header'>";
+      echo "<div class='user-left'>";
+      echo "<img src= 'pfp/" . $row['profilepic'] . "' alt='Foto Profile'>";
+      echo "</div>";
+      echo "<div class='user-info'>";
+      echo "<p class = 'display-name'>" . $row['nickname'] . "</p>";
+      echo "<p class = 'username'>@" . $row['username'] . "</p>";
+      echo "<br>" . "<p class = 'joined-date'>" . $row['created_at'] . "</p>";
+      echo "</div>";
+      echo "</div>";
+
+      echo "<label class = 'edit-toggle'>";
+      echo "<a href = '#'>Edit Profile</a>";
+      echo "</label>";
+      echo "</div>";
+      echo "</div>";
+
+
+      $id2 = $row['id'];
+      $query2 = "SELECT * FROM post WHERE user_id = '$id2'";
+      $result2 = mysqli_query($connection, $query2);
+      while ($row2 = mysqli_fetch_array($result2)) {
+        echo "<div class='posting_card'>";
+        echo "  <div class='user-header'>";
+        echo "    <div class='user-left'>";
+        echo "      <img src = 'pfp/" . $row2['profilepic'] . "' alt='Foto Profil'>";
+        echo "        <div class='user-info'>";
+        echo "          <p class='display-name'>" . $row2['nickname'] . "</p>";
+        echo "          <p class='username'>" . $row2['username'] . "</p>";
+        echo "        </div>";
+        echo "    </div>";
+
+        echo "    <label class='follow-toggle'>";
+        echo "      <input type='checkbox' hidden />";
+        echo "      <span class='follow-btn'>Follow</span>";
+        echo "    </label>";
+        echo "   </div>";
+        echo "  <img src = '../Posting/" . $row['gambar'] . "' class='post-image'>";
+
+        echo '<span>' . $row['caption'] . '</span><br>
+              <div class="button_action">
+              
+                <label class="icon-toggle">
+                  <input type="checkbox" name="likes" hidden>
+                  <span class="fa-regular fa-heart"></span>
+                  <span>' . $row['likes'] . '</span>
+                </label>
+
+                <label class="icon-toggle">
+                  <input type="checkbox" hidden>
+                  <span class="fa-regular fa-comment"></span>
+                </label>
+                <label class="icon-toggle">
+                  <input type="checkbox" hidden>
+                  <span class="fa-regular fa-bookmark"></span>
+                </label>
+                <label class="icon-toggle">
+                  <input type="checkbox" hidden>
+                  <span class="fa-solid fa-retweet"></span>
+                </label>
+              </div>
+            </div>';
+      }
     }
-    
-    }
-    ?>
+
+  }
+  ?>
 </body>
 
 </html>
