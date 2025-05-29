40e9e2f1abafe9becbe841acc1853fd9b012e66f
MrSushi74 <jameskoh313@gmail.com>
2025-05-29 22:52:55 +0700

    fixed bookmark bug, reverted bookmark table

--- Connection/CreateTableBookmark.php
+++ Connection/CreateTableBookmark.php
@@ -4,10 +4,7 @@
     $query = "CREATE TABLE bookmark (
         bookmark_id INT PRIMARY KEY AUTO_INCREMENT,
         post_id INT NOT NULL,
-        user_id INT NOT NULL,
-        caption VARCHAR(50) NOT NULL,
-        gambar VARCHAR(100),
-        likes INT DEFAULT 0
+        user_id INT NOT NULL
     );";
 
     $result = mysqli_query($connection,$query);
