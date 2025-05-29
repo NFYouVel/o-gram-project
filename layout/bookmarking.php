2942e83af16d2a313d9f86db486c884028fb99f2
MrSushi74 <jameskoh313@gmail.com>
2025-05-29 21:30:21 +0700

    Finished bookmark, added new fields to post table , bookmark table.

---
+++ layout/bookmarking.php
@@ -0,0 +1,25 @@
+<?php
+    include "../Connection/Connection.php";
+    session_start();
+    $user_id = $_SESSION["user_id"];
+    $post_id = intval($_POST['postid']);
+    $caption = $_POST["caption"];
+    $image = $_POST["image"];
+    $likes = $_POST["likes"];
+
+    $check = mysqli_query($connection, "SELECT 1 FROM bookmark WHERE user_id = $user_id AND post_id = $post_id");
+    if (mysqli_num_rows($check) > 0) {
+    // Hapus bookmark
+    mysqli_query($connection, "DELETE FROM bookmark WHERE user_id = $user_id AND post_id = $post_id");
+    mysqli_query($connection, "UPDATE post SET bookmarked = bookmarked - 1 WHERE post_id = $post_id");
+    } else {
+    // Tambahkan bookmark
+    mysqli_query($connection, "INSERT INTO bookmark (post_id, user_id, caption, gambar, likes) VALUES ($post_id, $user_id, '$caption', '$image', $likes)");
+    mysqli_query($connection, "UPDATE post SET bookmarked = bookmarked + 1 WHERE post_id = $post_id");
+    }
+    $takeBookmark = mysqli_query($connection,"SELECT bookmarked FROM post WHERE post_id = $post_id");
+    $row = mysqli_fetch_array($takeBookmark);
+    echo $row["bookmarked"];
+
+
+?>
